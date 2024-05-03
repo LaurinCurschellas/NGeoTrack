b629c473a64e3e708ae3fa1fadb88993f35a85f4
LaurinCurschellas <126350325+LaurinCurschellas@users.noreply.github.com>
2024-05-03 11:13:30 +0200

    data and description

---
+++ R/data.R
@@ -0,0 +1,61 @@
+#' Shape File Municipalities of Norway (2021)
+#'
+#' A data series by the Norwegian mapping authority, released on 'GEONORGE'.
+#'
+#' @format ## `shp_mun_2021`
+#' A data frame with 363 rows and 3 columns:
+#' \describe{
+#'   \item{kommune}{Municipality Identifier (Not Harmonized)}
+#'   \item{year}{Year}
+#'   \item{geometry}{A sfc_Polygon, spacial definition of municipalities}
+#'   ...
+#' }
+#' @source <https://kartkatalog.geonorge.no/>
+"shp_mun_2021"
+
+
+#' Shape File Municipalities of Norway (2018)
+#'
+#' A data series by the Norwegian mapping authority, released on 'GEONORGE'.
+#'
+#' @format ## `shp_mun_2018`
+#' A data frame with 363 rows and 3 columns:
+#' \describe{
+#'   \item{kommune}{Municipality Identifier (Not Harmonized)}
+#'   \item{year}{Year}
+#'   \item{geometry}{A sfc_Polygon, spacial definition of municipalities}
+#'   ...
+#' }
+#' @source <https://kartkatalog.geonorge.no/>
+"shp_mun_2018"
+
+
+#' Shape File Basic Statistical Units (BSU) of Norway (2018)
+#'
+#' A data series by the Norwegian mapping authority, released on 'GEONORGE'.
+#'
+#' @format ## `shp_bsu_2018`
+#' A data frame with 14'058 rows and 3 columns:
+#' \describe{
+#'   \item{grunnkrets}{BSU Identifier (Not Harmonized)}
+#'   \item{year}{Year}
+#'   \item{geometry}{A sfc_Polygon, spacial definition of BSU's}
+#'   ...
+#' }
+#' @source <https://kartkatalog.geonorge.no/>
+"shp_bsu_2018"
+
+#' Shape File Basic Statistical Units (BSU) of Norway (2021)
+#'
+#' A data series by the Norwegian mapping authority, released on 'GEONORGE'.
+#'
+#' @format ## `shp_bsu_2021`
+#' A data frame with 14'102 rows and 3 columns:
+#' \describe{
+#'   \item{grunnkrets}{BSU Identifier (Not Harmonized)}
+#'   \item{year}{Year}
+#'   \item{geometry}{A sfc_Polygon, spacial definition of BSU's}
+#'   ...
+#' }
+#' @source <https://kartkatalog.geonorge.no/>
+"shp_bsu_2021"
