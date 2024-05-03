7dccba6a54c268eecdd4a4003fd4c64b332d7d99
LaurinCurschellas <126350325+LaurinCurschellas@users.noreply.github.com>
2024-05-03 14:11:40 +0200

    dplyr as Depends: instead of Imports:

--- R_Validation/test.R
+++ R_Validation/test.R
@@ -3,19 +3,20 @@ library(roxygen2)
 
 devtools::install_github("https://github.com/LaurinCurschellas/NGeoTrack")
 library(NGeoTrack)
-library(dplyr)
+#library(dplyr)
 
-change_bsu <- gather_change(type = "grunnkrets", from = 1999, to = 2015)
-status_bsu <- status_quo(type = "grunnkrets", from = 1999, to = 2015)
+change_bsu <- gather_change(type = "grunnkrets", from = 1999, to = 2018)
+status_bsu <- status_quo(type = "grunnkrets", from = 1999, to = 2018)
 
-status_mun <- status_quo(type = "kommune", from = 1999, to = 2015)
-change_mun <- gather_change(type = "kommune", from = 1999, to = 2015)
+status_mun <- status_quo(type = "kommune", from = 1999, to = 2018)
+change_mun <- gather_change(type = "kommune", from = 1999, to = 2018)
 
 
 stat_list <- list(status_mun, status_bsu)
 chg_list <- list(change_bsu, change_mun)
 
 
-key_joint <- EtE_changes(df_status = stat_list, df_change = chg_list, from = 1999 , to = 2015, jointly = TRUE)
+key_joint <- EtE_changes(df_status = stat_list, df_change = chg_list, from = 1999 , to = 2018, jointly = TRUE)
 
-check_overlap(key_joint)
+overlap <- check_overlap(key_joint)
+
