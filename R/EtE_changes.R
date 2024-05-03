8bff92886167038ee1a104abf15489a8418a7784
LaurinCurschellas <126350325+LaurinCurschellas@users.noreply.github.com>
2024-05-03 13:17:29 +0200

    igraph dep

--- R/EtE_changes.R
+++ R/EtE_changes.R
@@ -16,8 +16,8 @@ coerce_shape <- function(df, change = TRUE) {
 
   if (change) {
 
-    df <- df %>%
-      dplyr::select(from, to, oldName , newName , year) %>%
+    df <- df |>
+      dplyr::select(from, to, oldName , newName , year) |>
       dplyr::mutate(
         from = as.integer(from),
         to = as.integer(to),
@@ -26,7 +26,7 @@ coerce_shape <- function(df, change = TRUE) {
 
   } else {
 
-    df <- df %>%
+    df <- df |>
       dplyr::mutate(
         geoID = as.integer(geoID)
       )
@@ -72,7 +72,7 @@ EtE_changes <- function(df_status ,df_change ,from , to, jointly = FALSE) {
     # Coerce into form:
     df_change <- coerce_shape(df_change, change = TRUE)
     # Filter based on paramter 'to' and NOT the variable 'to'
-    df_status <- coerce_shape(df_status, change = FALSE) %>%
+    df_status <- coerce_shape(df_status, change = FALSE) |>
       dplyr::filter(from <= ({{to}}-1))
 
     # Handle Case of no changes - in such a period the key is equal to the status
@@ -88,41 +88,41 @@ EtE_changes <- function(df_status ,df_change ,from , to, jointly = FALSE) {
       # Y: Cluster_id (allows for up to 99 thousand changes))
 
       df_members <- data.frame(
-        name = as.integer(V(graph)$name),
+        name = as.integer(igraph::V(graph)$name),
         cluster = clusters$membership,
         row.names = NULL
-      ) %>%
+      ) |>
         dplyr::mutate(
           cluster_id = as.integer(1*10^5+cluster)
-        ) %>%
-        dplyr::select(-cluster) %>%
+        ) |>
+        dplyr::select(-cluster) |>
         dplyr::distinct()
 
 
 
       EtE_key <- dplyr::left_join(df_status , df_members, dplyr::join_by("geoID" == "name"))
-      EtE_key <- EtE_key %>%
+      EtE_key <- EtE_key |>
         dplyr::mutate(
           cluster_id = ifelse(is.na(cluster_id) , geoID , cluster_id ),
           year = from
-        ) %>%
+        ) |>
         dplyr::select(-c(from,to))
 
     } else {
 
       # This else statement refers to an empty df_change dataframe, in periods with no change
-      EtE_key <- df_status %>%
+      EtE_key <- df_status |>
         dplyr::mutate(
           geoID = geoID,
           name = name,
           cluster_id = geoID,
           year = from
-        )%>%
+        )|>
         dplyr::select(-c(from,to))
 
     }
 
-    EtE_key <- EtE_key %>%
+    EtE_key <- EtE_key |>
       dplyr::arrange(year)
 
   }
@@ -132,9 +132,9 @@ EtE_changes <- function(df_status ,df_change ,from , to, jointly = FALSE) {
     # Extract from supplied list (municipal data is always smaller than the grunnkrets) and coerse into shape
     df_change_bsu <- coerce_shape(nrow_find(df_change , max = TRUE), change = TRUE)
     df_change_mun <- coerce_shape(nrow_find(df_change , max = FALSE), change = TRUE)
-    df_status_bsu <- coerce_shape(nrow_find(df_status , max = TRUE), change = FALSE) %>%
+    df_status_bsu <- coerce_shape(nrow_find(df_status , max = TRUE), change = FALSE) |>
       dplyr::filter(from <= ({{to}}-1))
-    df_status_mun <- coerce_shape(nrow_find(df_status , max = FALSE), change = FALSE) %>%
+    df_status_mun <- coerce_shape(nrow_find(df_status , max = FALSE), change = FALSE) |>
       dplyr::filter(from <= ({{to}}-1))
 
 
@@ -142,14 +142,14 @@ EtE_changes <- function(df_status ,df_change ,from , to, jointly = FALSE) {
     # Have to be handled seperately
     if (nrow(df_change_bsu) == 0) {
 
-      EtE_key_bsu <- df_status_bsu %>%
+      EtE_key_bsu <- df_status_bsu |>
         dplyr::mutate(
           geoID = geoID,
           # Name and code have to be labelled for grunnkrets and municipalities
           Gname = name,
           Gcluster_id = geoID,
           year = from
-        )%>%
+        )|>
         dplyr::select(-c(from,to))
 
     } else {
@@ -159,35 +159,35 @@ EtE_changes <- function(df_status ,df_change ,from , to, jointly = FALSE) {
       clusters_bsu <- igraph::components(graph_bsu, mode = c("strong"))
 
       df_members_bsu <- data.frame(
-        Gname = as.integer(V(graph_bsu)$name),
+        Gname = as.integer(igraph::V(graph_bsu)$name),
         cluster = clusters_bsu$membership,
         row.names = NULL
-      ) %>%
+      ) |>
         dplyr::mutate(
           Gcluster_id = as.integer(1*10^5+cluster)
-        ) %>%
-        dplyr::select(-cluster) %>%
+        ) |>
+        dplyr::select(-cluster) |>
         dplyr::distinct()
 
-      EtE_key_bsu <- left_join(df_status_bsu , df_members_bsu, join_by("geoID" == "Gname"))
-      EtE_key_bsu <- EtE_key_bsu %>%
+      EtE_key_bsu <- dplyr::left_join(df_status_bsu , df_members_bsu, dplyr::join_by("geoID" == "Gname"))
+      EtE_key_bsu <- EtE_key_bsu |>
         dplyr::mutate(
           Gcluster_id = ifelse(is.na(Gcluster_id) , geoID , Gcluster_id ),
           Gname = name,
           year = from
-        ) %>%
+        ) |>
         dplyr::select(-c(from,to,name))
     }
 
     if (nrow(df_change_mun) == 0) {
 
-      EtE_key_mun <- df_status_mun %>%
+      EtE_key_mun <- df_status_mun |>
         dplyr::mutate(
           geoID = geoID,
           Cname = name,
           Ccluster_id = geoID,
           year = from
-        )%>%
+        )|>
         dplyr::select(-c(from,to))
 
     } else {
@@ -201,37 +201,37 @@ EtE_changes <- function(df_status ,df_change ,from , to, jointly = FALSE) {
       # Y: Cluster_id (allows for up to 99 thousand changes))
 
       df_members_mun <- data.frame(
-        Cname = as.integer(V(graph_mun)$name),
+        Cname = as.integer(igraph::V(graph_mun)$name),
         cluster = clusters_mun$membership,
         row.names = NULL
-      ) %>%
+      ) |>
         dplyr::mutate(
           Ccluster_id = as.integer(5*10^5+cluster)
-        ) %>%
-        dplyr::select(-cluster) %>%
+        ) |>
+        dplyr::select(-cluster) |>
         dplyr::distinct()
 
       EtE_key_mun <- dplyr::left_join(df_status_mun , df_members_mun, dplyr::join_by("geoID" == "Cname"))
-      EtE_key_mun <- EtE_key_mun %>%
+      EtE_key_mun <- EtE_key_mun |>
         dplyr::mutate(
           Ccluster_id = ifelse(is.na(Ccluster_id) , geoID , Ccluster_id ),
           Cname = name,
           year = from
-        ) %>%
+        ) |>
         dplyr::select(-c(from,to,name))
 
     }
 
     ## Merge the two keys into one joint key
 
-    EtE_key_bsu <- EtE_key_bsu %>%
+    EtE_key_bsu <- EtE_key_bsu |>
       dplyr::mutate(
         str_l = as.integer(nchar(geoID)),
         # Municipality id are either the first 3, or 4 digits depending on the length
         # of the municipality code (and thus the length of the entire code)
         mun_id = ifelse(str_l != 7 , as.integer(substr(geoID, 0, 4)),
                         as.integer(substr(geoID, 0, 3)))
-      ) %>%
+      ) |>
       dplyr::select(-c(str_l))
 
 
@@ -240,7 +240,7 @@ EtE_changes <- function(df_status ,df_change ,from , to, jointly = FALSE) {
 
   }
 
-  EtE_key <- EtE_key %>%
+  EtE_key <- EtE_key |>
     dplyr::arrange(year)
 
   return(EtE_key)
--- R_Validation/test.R
+++ R_Validation/test.R
@@ -1,8 +1,9 @@
 library(devtools)
 library(roxygen2)
 
-devtools::install_local("C:/Users/s16062/OneDrive - Norwegian School of Economics/Dokumenter/03_Packages/NGeoTrack_0.1.0.tar.gz")
+devtools::install_github("https://github.com/LaurinCurschellas/NGeoTrack")
 library(NGeoTrack)
+library(dplyr)
 
 change_bsu <- gather_change(type = "grunnkrets", from = 1999, to = 2015)
 status_bsu <- status_quo(type = "grunnkrets", from = 1999, to = 2015)
@@ -15,6 +16,6 @@ stat_list <- list(status_mun, status_bsu)
 chg_list <- list(change_bsu, change_mun)
 
 
-key_joint <- EtE_changes(df_status = stat_list, df_change = chg_list, from = 1999 , to = 2015, jointly = FALSE)
+key_joint <- EtE_changes(df_status = stat_list, df_change = chg_list, from = 1999 , to = 2015, jointly = TRUE)
 
 check_overlap(key_joint)
