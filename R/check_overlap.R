6cc4df5ceef9183dc745a65ad16944607e917c85
Laurin L. Curschellas <126350325+LaurinCurschellas@users.noreply.github.com>
2024-05-14 11:07:13 +0200

    extension

--- R/check_overlap.R
+++ R/check_overlap.R
@@ -30,16 +30,28 @@
 #'   }
 #' }
 #' @export
-check_overlap <- function(df_key ) {
+check_overlap <- function(df_key ) { 
+
+  # Recognise the supplied types: 
+  constr <- attributes(df_key)$comment
+  components <- strsplit(constr, "-")[[1]]
+
+  bot_type <- components[1]
+  top_type <- components[2]
+  
+  # Unify the names of the columns across different types of keys
+  # The names are added back in to the output at the end
+  internal <- df_key
+  colnames(internal) <- c("geoID", "botName", "botID", "year", "topgeoID", "topName", "topID")
 
   test <- df_key %>%
-    dplyr::group_by(Gcluster_id, year) %>%
+    dplyr::group_by(botID, year) %>%
     dplyr::mutate(
-      n_Mun = length(unique(Ccluster_id))
+      n_Top = length(unique(topID))
     ) %>%
     dplyr::ungroup()
 
-  candidates <- unique(test$Gcluster_id[test$n_Mun > 1])
+  candidates <- unique(test$botID[test$n_Top > 1])
 
   if (length(candidates) == 0) {
 
@@ -55,20 +67,39 @@ check_overlap <- function(df_key ) {
   num_candidates <- length(candidates)
 
   test <- test %>%
-    dplyr::filter(n_Mun != 1) %>%
-    dplyr::select(c(Gcluster_id, Ccluster_id, Cname, year)) %>%
-    dplyr::distinct(Gcluster_id, Ccluster_id , Cname, .keep_all = TRUE) %>%
-    dplyr::group_by(Gcluster_id, Ccluster_id) %>% mutate(max_year = max(year)) %>% ungroup() %>%
+    dplyr::filter(n_Top != 1) %>%
+    dplyr::select(c(botID, topID, topName, year)) %>%
+    dplyr::distinct(botID, topID , topName, .keep_all = TRUE) %>%
+    dplyr::group_by(botID, topID) %>% mutate(max_year = max(year)) %>% ungroup() %>%
     dplyr::filter(max_year == year) %>%
     dplyr::select(-c(year, max_year)) %>%
-    dplyr::group_by(Gcluster_id) %>%
+    dplyr::group_by(botID) %>%
     dplyr::mutate(
       name = c(seq(1,n()))
     ) %>%
     dplyr::ungroup()
 
-  test <- tidyr::pivot_wider(test, names_from = name, values_from = c(Ccluster_id, Cname))
+  test <- tidyr::pivot_wider(test, names_from = name, values_from = c(topID, topName))
 
+  # Redefine the Column names of the output, based on the supplied case: 
+  botID <- switch(
+    bot_type,
+    grunnkrets = "Gcluster_id",
+    kommune = "Ccluster_id"
+  )
+
+  top_prefix <- switch(
+    top_type,
+    kommune = "C",
+    fylket = "F",
+  )
+
+  topID <- paste(top_prefix, "cluster_id", sep = "")
+  topName <- paste(top_prefix, "name", sep = "")  
+  NewColnames <- c(botID, paste(topID, "_1", sep = ""), paste(topID, "_2", sep = ""),
+                   paste(topName, "_1", sep = ""), paste(topName, "_2", sep = ""))
+  colnames(test) <- NewColnames
+    
   # Print the wide-format data frame
   d <- Sys.setlocale(category = "LC_ALL", locale = "en_US.UTF-8") # System has to be able to read the UTF-8 characters
 
